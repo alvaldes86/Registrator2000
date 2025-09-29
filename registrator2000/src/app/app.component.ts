44e82e834cdb13cded49e55826c59448864da2b2
alvaldes86 <76670909+alvaldes86@users.noreply.github.com>
2025-09-29 14:31:53 -0400

    components

--- registrator2000/src/app/app.component.ts
+++ registrator2000/src/app/app.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { RegistrationServiceService } from './services/registration-service.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+
   title = 'registrator2000';
+
+  posts: any[] = [];   // will hold API response
+  loading = true;      // simple loading flag
+  error: string | null = null;
+
+  private registrationService = inject(RegistrationServiceService);
+
+  ngOnInit(): void {
+    // Call the service and log the result
+    this.registrationService.getWithHeaders().subscribe({
+      next: (response) => {
+        console.log('API response:', response);
+      },
+      error: (error) => {
+        console.error('Error from API:', error);
+      }
+    });
+  }
 }
---
+++ registrator2000/src/app/services/registration-service.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RegistrationServiceService {
+
+  constructor(private http: HttpClient) { }
+
+  getMessage(): string {
+    return 'Hello from MyService!';
+  }
+
+  private apiUrl = 'https://enrollpro-ctdeb9a9a9c8b8ez.canadacentral-01.azurewebsites.net/business/getAllBusinesses';
+  getWithHeaders(): Observable<any> {
+    return this.http.get(this.apiUrl, {
+      headers: {
+        'Content-Type': 'application/json',
+        'x-api-key': '0d2e643e-e9b4-4b1a-bb7a-e717f4ae1f5d'
+      }
+    });
+  }
+
+  getProducts(): Observable<any> {
+    return this.http.get('https://simple-grocery-store-api.glitch.me/products');
+  }
+}
