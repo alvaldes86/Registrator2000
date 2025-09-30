e02f8efd6ed377a8afca11c990f0f8bd9ec38b1c
alvaldes86 <76670909+alvaldes86@users.noreply.github.com>
2025-09-30 09:19:23 -0400

    Improve mobile navbar icons

--- registrator2000/src/app/components/navbar/navbar.component.ts
+++ registrator2000/src/app/components/navbar/navbar.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule, RouterLink, RouterLinkActive } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink, RouterLinkActive, RouterModule],
+  imports: [RouterLink, RouterLinkActive, RouterModule, MatIconModule],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
